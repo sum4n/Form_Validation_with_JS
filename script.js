6f2a68417485bff1a6dc6490d2017c086f00b721
Suman Das <sum4ndas@gmail.com>
2022-10-02 22:50:05 +0530

    Add eventListeners to individual inputs and add required attribute. Add showError() conditions for empty fields.

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const form = document.querySelector("form");
 const mail = document.getElementById("mail");
 const mailError = document.querySelector(".mail-error");
 const country = document.getElementById("country");
-
+const countryError = document.querySelector(".country-error");
+const zip = document.getElementById("zip");
+const zipError = document.querySelector(".zip-error");
+const password = document.getElementById("password");
+const passwordError = document.querySelector(".password-error");
+const confirmPassword = document.getElementById("confirm-password");
+const confirmPasswordError = document.querySelector(".confirmPassword-error");
 
 const submitButton = document.querySelector("button");
 
 
-mail.addEventListener("input", (event) => {
+mail.addEventListener("input", () => {
     if (mail.validity.valid) {
         mailError.textContent = "";
     } else {
@@ -16,8 +22,52 @@ mail.addEventListener("input", (event) => {
     }
 });
 
+country.addEventListener("input", () => {
+    if (country.validity.valid) {
+        countryError.textContent = "";
+    } else {
+        showError();
+    }
+});
+
+zip.addEventListener("input", () => {
+    if (zip.validity.valid) {
+        zipError.textContent = "";
+    } else {
+        showError();
+    }
+});
+
+password.addEventListener("input", () => {
+    if (password.validity.valid) {
+        passwordError.textContent = "";
+    } else {
+        showError();
+    }
+});
+
+confirmPassword.addEventListener("input", () => {
+    if (confirmPassword.validity.valid) {
+        confirmPasswordError.textContent = "";
+    } else {
+        showError();
+    }
+});
+
 form.addEventListener("submit", (event) => {
     if (!mail.validity.valid) {
+        showError();
+        event.preventDefault();
+    } else if (!country.validity.valid) {
+        showError();
+        event.preventDefault();
+    } else if (!zip.validity.valid) {
+        showError();
+        event.preventDefault();
+    } else if (!password.validity.valid) {
+        showError();
+        event.preventDefault();
+    } else if (!confirmPassword.validity.valid) {
         showError();
         event.preventDefault();
     }
@@ -28,5 +78,18 @@ function showError() {
         mailError.textContent = "You need to enter an email address.";
     } else if (mail.validity.typeMismatch) {
         mailError.textContent = "Entered value needs to be an email address.";
+    }
+    if (country.validity.valueMissing) {
+        countryError.textContent = "You need to enter a country name.";
+    }
+    if (zip.validity.valueMissing) {
+        zipError.textContent = "You need to enter a zip code.";
+    }
+    if (password.validity.valueMissing) {
+        passwordError.textContent = "You need to enter a password.";
+    }
+    
+    if (confirmPassword.validity.valueMissing) {
+        confirmPasswordError.textContent = "You need to enter a matching password.";
     }
 }
